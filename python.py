ab96d5036f81cef5533c77da0fc0fbf20f6be8cd
Lekden4412 <tibetsmonk@gmail.com>
2025-09-02 15:50:54 +0530

    agin

--- python.py
+++ python.py
@@ -4,4 +4,5 @@
 #space is very important in python so naver add space before or after = sign
 #print("tibtan are not coder" )
 #input ()function
-print("hello " + input("what is your name? ")) #input function always return string value
+#print("hello " + input("what is your name? ")) #input function always return string value
+print("Tashi Delek " + input("What is your name?")) #string multiplication

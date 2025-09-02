7507c2a21d45d65bd83236e754b55f8a89928c0e
Lekden4412 <tibetsmonk@gmail.com>
2025-09-02 15:05:41 +0530

    more

--- python.py
+++ python.py
@@ -1,4 +1,5 @@
 #print is simple output function what you can use to display text on the screen
-print ("Hello World\nHello Python")
-print ("Hello" + " Python") #string concatenation
-
+print("Hello World\nHello Python")
+print("Hello" + " Python") #string concatenation
+#space is very important in python so naver add space before or after = sign
+print("tibtan are not coder" )

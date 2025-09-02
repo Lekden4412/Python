cbb78b2d096f9f6a4791b2fa0f69375e3598c671
Lekden4412 <tibetsmonk@gmail.com>
2025-09-02 15:46:52 +0530

    input code

--- python.py
+++ python.py
@@ -1,5 +1,7 @@
 #print is simple output function what you can use to display text on the screen
-print("Hello World\nHello Python")
-print("Hello" + " Python") #string concatenation
+#print("Hello World\nHello Python")
+#print("Hello" + " Python") #string concatenation
 #space is very important in python so naver add space before or after = sign
-print("tibtan are not coder" )
+#print("tibtan are not coder" )
+#input ()function
+print("hello " + input("what is your name? ")) #input function always return string value

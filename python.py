9e4ab7e30652862189acd62b61ba2e795a7996e8
Lekden4412 <tibetsmonk@gmail.com>
2025-09-02 14:59:54 +0530

    more

--- python.py
+++ python.py
@@ -1,4 +1,4 @@
 #print is simple output function what you can use to display text on the screen
-print ("Hello World")
-
+print ("Hello World\nHello Python")
+print ("Hello" + " Python") #string concatenation
 

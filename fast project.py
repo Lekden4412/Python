052783a131bccff28a4b1e1783505bfe245dea8f
Lekden4412 <tibetsmonk@gmail.com>
2025-09-05 00:26:53 +0530

    add more code

--- fast project.py
+++ fast project.py
@@ -4,3 +4,8 @@ city = input("")
 print("what is your pet name?")
 pet = input("")
 print("your band name clould be " + city + " " + pet)
+#secone code is more easy 
+print("welcome to my fast project")
+city = input("what is your city name? \n")
+pet = input("what is your pet name?\n")
+print("your band name is " + pet + " " + city)

dca3739d338bf05f9015a3454b78cda4dcb2573b
Lekden4412 <tibetsmonk@gmail.com>
2025-09-07 00:05:14 +0530

    BMI add

---
+++ BMI.py
@@ -0,0 +1,9 @@
+
+height = input("what is you height " )
+weight = input("what is your weght ")
+Integers_float = float(height)
+Integers_weight = int(weight)
+height_multiple = Integers_float * Integers_float
+weight_dived = Integers_weight / height_multiple
+bmi = weight_dived
+print(bmi)
